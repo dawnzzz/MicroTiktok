7cc836590af2ad4e4b5083f9dab4642857f5d6b3
Dawnzzz <635441876@qq.com>
2023-05-07 19:43:53 +0800

    debug

--- internal/api/util/convert.go
+++ internal/api/util/convert.go
@@ -7,20 +7,16 @@ import (
 
 func RpcUserConvertToApiUser(rpcUser *rpcbase.User) *apibase.User {
 	return &apibase.User{
-		ID:   rpcUser.Id,
-		Name: rpcUser.Name,
-		SocialInfo: &apibase.SocialInfo{
-			FollowCount:   rpcUser.SocialInfo.FollowCount,
-			FollowerCount: rpcUser.SocialInfo.FollowerCount,
-			IsFollow:      rpcUser.SocialInfo.IsFollow,
-		},
+		ID:              rpcUser.Id,
+		Name:            rpcUser.Name,
+		FollowCount:     rpcUser.FollowCount,
+		FollowerCount:   rpcUser.FollowerCount,
+		IsFollow:        rpcUser.IsFollow,
 		Avatar:          rpcUser.Avatar,
 		BackgroundImage: rpcUser.BackgroundImage,
 		Signature:       rpcUser.Signature,
-		UserInteracInfo: &apibase.UserInteractInfo{
-			TotalFavorited: rpcUser.UserInteracInfo.TotalFavorited,
-			WorkCount:      rpcUser.UserInteracInfo.WorkCount,
-			FavoriteCount:  rpcUser.UserInteracInfo.FavoriteCount,
-		},
+		TotalFavorited:  rpcUser.TotalFavorited,
+		WorkCount:       rpcUser.WorkCount,
+		FavoriteCount:   rpcUser.FavoriteCount,
 	}
 }
